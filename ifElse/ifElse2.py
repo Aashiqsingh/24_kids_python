119371bb2704d35765b2cba68d3a0525d489a57e
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-09-20 18:46:28 +0530

    if else , task

--- ifElse/ifElse2.py
+++ ifElse/ifElse2.py
@@ -15,17 +15,30 @@
 #     print("B is gretter")
 # else:
 #     print("C is gretter")
-a = 25
-b = 22
-c = 30
+# a = 25
+# b = 22
+# c = 30
 
-if a > b:
-    if a > c:
-        print("A is gretter")
-    else:
-        print("C is gretter")
-else:
-    if b > c:
-        print("B is gretter")
-    else:
-        print("C is gretter")
+# if a > b:
+#     if a > c:
+#         print("A is gretter")
+#     else:
+#         print("C is gretter")
+# else:
+#     if b > c:
+#         print("B is gretter")
+#     else:
+#         print("C is gretter")
+
+
+# amount  : 4897 
+
+# 500 - 9 
+# 200 - 1 
+# 100 - 1
+# 50 - 1
+# 20 - 2 
+# 10  - 0 
+# 5 - 1 
+# 2 - 1 
+# 1 - 0  
