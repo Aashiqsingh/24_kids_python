499936ba4700abab939a56c2e03ddea6cedf7b5b
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-09-20 18:05:43 +0530

    if else

---
+++ ifElse/ifElse1.py
@@ -0,0 +1,22 @@
+a = 19
+b = 17
+c = 18
+
+
+#  a > b ? "a is greater" : "b is greater"
+
+# ans = "a is gretter" if a > b else "b is greater"
+# ans = a if a > b else b
+# print(ans)
+
+# print(a) if a > b else print(b)
+
+# ans = "A is gretter" if a > b and a > c else "B is gretter" if b > c and b > a else "C is gretter"
+# print(ans)
+
+
+isALive = True 
+age = 19
+
+ans = "Alive" if isALive == True else "Eligible of vote" if age > 91  else "Not eligible of vote"
+print(ans)
---
+++ ifElse/ifElse2.py
@@ -0,0 +1,31 @@
+# a = 13
+# b = 17
+
+# if a > b:
+#     print("A is gretter")
+# else:
+#     print("B is gretter")
+
+
+
+
+# if a > b and a > c:
+#     print("A is gretter")
+# elif b > c and b > a:
+#     print("B is gretter")
+# else:
+#     print("C is gretter")
+a = 25
+b = 22
+c = 30
+
+if a > b:
+    if a > c:
+        print("A is gretter")
+    else:
+        print("C is gretter")
+else:
+    if b > c:
+        print("B is gretter")
+    else:
+        print("C is gretter")
