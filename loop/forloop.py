b280852320e1b950c3f34083dc298ecae206c592
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-09-28 15:16:29 +0530

    for loop

---
+++ loop/forloop.py
@@ -0,0 +1,25 @@
+# for i in range(10):
+#     print(i)
+
+# for i in range(1,10):
+#     print(i,end=" ")
+
+# for i in range(1,10+1):
+#     print(i)
+
+# for i in range(2,10+1,2):
+#     print(i)
+
+# for i in range(10,0,-2):
+#     print(i)
+
+
+# for i in range(10):
+#     print("Hello")
+
+
+num = 5 
+fact = 1
+for i in range(1,num+1):
+    fact = fact * i
+print(fact)
