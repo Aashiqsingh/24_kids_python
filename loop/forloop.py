2bfb79945db8243e47b2e4a1261eca17b9c7a099
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-10-04 17:39:21 +0530

    square

--- loop/forloop.py
+++ loop/forloop.py
@@ -18,8 +18,8 @@
 #     print("Hello")
 
 
-num = 5 
-fact = 1
-for i in range(1,num+1):
-    fact = fact * i
-print(fact)
+# num = 5 
+# fact = 1
+# for i in range(1,num+1):
+#     fact = fact * i
+# print(fact)
---
+++ loop/forloop2.py
@@ -0,0 +1,25 @@
+# for i in range(5):
+#     print(i) 
+
+# for i in range(1,5+1):
+#     # print(i) 
+#     print("*",end=" ")
+
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print("*",end=" ")
+#     print("\n")
+
+# num = int(input("Enter the value"))
+# for i in range(1,num+1):
+#     for j in range(1,num+1):
+#         print(j,end=" ")
+#     print("\n")
+
+for i in range(1,6):
+    for j in range(1,6):
+        if i == 1 or j == 1 or i == 5 or j == 5:
+            print("*",end=" ")
+        else: 
+            print(end="  ")
+    print("\n")
