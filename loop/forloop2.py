95370a98f8a2c3c5556c4e989e5181a4b1dbeb19
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-10-05 15:34:00 +0530

    cross pattern

--- loop/forloop2.py
+++ loop/forloop2.py
@@ -34,7 +34,30 @@
 
 
 
-for i in range(1,6):
-    for j in range(1,i+1):
-        print(i,end=" ")
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(i,end=" ")
+#     print("\n")
+
+
+num = int(input("Enter the value"))
+
+for i in range(1,num+1):
+    for j in range(1,num+1):
+        if i == j or i + j == num+1:
+            print("*",end=" ")
+        else:
+            print(end="  ")
     print("\n")
+
+
+
+# num = int(input("Enter the value"))
+
+# for i in range(1,num+1):
+#     for j in range(1,num+1):
+#         if i == j or i + j == num+1 or i == 1 or j == 1 or i == num or j == num:
+#             print("*",end=" ")
+#         else:
+#             print(end="  ")
+#     print("\n")
